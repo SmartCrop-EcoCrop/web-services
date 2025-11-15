798dec2c757ffd3252567ccab0f43ba53bb41f60
Sebastain Ramirez Tello <sebastianramte22@gmail.com>
2025-11-15 17:36:43 -0500

    feat(cors): Habilita acceso desde localhost:4200 para el frontend de Angular

---
+++ src/main/java/com/ecocrop/smartcropwebservices/config/CorsConfig.java
@@ -0,0 +1,17 @@
+﻿package com.ecocrop.smartcropwebservices.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**") // Aplica a todos tus endpoints
+                .allowedOrigins("http://localhost:4200", "https://tu-dominio-angular.com") // Añade tu dominio local y de despliegue
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Permite los métodos que usas
+                .allowedHeaders("*"); // Permite todos los encabezados
+    }
+}
